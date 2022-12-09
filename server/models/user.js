639504665157252d5b2413ebfd1c3e9579d79df2
ElizavetaZh <elisaveta.zhigunova@yandex.ru>
2022-12-09 15:08:19 +0300

    09/12/22

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+const {Schema, model} = require("mongoose");
+
+const User = new Schema({
+    name: {type: String, required: true, unique: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    avatar: {type: String},
+    ad: {type: String, default: 0}
+})
+
+module.exports = model('User', User)
